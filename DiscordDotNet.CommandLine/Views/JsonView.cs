07902b3161ccd03b9b82f6ca41667340f6ef0a9f
Garrett Leach <garrett.d.leach@gmail.com>
2021-04-25 23:57:19 -0500

    Add a JSON view
    This makes formatting JSON to the console much simpler. The approach seems rather simple and elegant to me.

---
+++ DiscordDotNet.CommandLine/Views/JsonView.cs
@@ -0,0 +1,16 @@
+﻿using System.CommandLine.Rendering.Views;
+using System.Text.Json;
+
+namespace DiscordDotNet.CommandLine.Views
+{
+    internal class JsonView<T> : StackLayoutView
+    {
+        public JsonView(T obj)
+        {
+            foreach (var line in JsonSerializer.Serialize(obj, typeof(T), new JsonSerializerOptions() { WriteIndented = true }).Split("\r\n"))
+            {
+                Add(new ContentView(line));
+            }
+        }
+    }
+}
