73ac954602667f44827dfbf7d158d385feded09a
Garrett Leach <garrett.d.leach@gmail.com>
2021-04-27 22:25:30 -0500

    Add cancellation support
    This is the most simplistic method for doing so but the results are quite good. When cancelling on a request the request was cancelled and no output was written to the console.

--- src/DiscordDotNet.CommandLine/Commands/ApplicationsGuildsCommandsGetCommand.cs
+++ src/DiscordDotNet.CommandLine/Commands/ApplicationsGuildsCommandsGetCommand.cs
@@ -8,6 +8,7 @@ using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.Rendering;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DiscordDotNet.CommandLine.Commands
@@ -18,21 +19,21 @@ namespace DiscordDotNet.CommandLine.Commands
         {
             var commandIdOpt = new Option<string>("--command-id", "The command id to request (omit for all)", ArgumentArity.ZeroOrOne);
             AddOption(commandIdOpt);
-            Handler = CommandHandler.Create<InvocationContext, IHost, GuildApplicationOptions, string>(Exec);
+            Handler = CommandHandler.Create<InvocationContext, IHost, GuildApplicationOptions, CancellationToken, string>(Exec);
         }
-        private async Task<int> Exec(InvocationContext invocationContext, IHost host, GuildApplicationOptions guildAppOptions, string commandId = null)
+        private async Task<int> Exec(InvocationContext invocationContext, IHost host, GuildApplicationOptions guildAppOptions, CancellationToken cancellationToken, string commandId = null)
         {
             var client = host.Services.GetRequiredService<IDiscordWebClient>();
 
             if (commandId is string commId)
             {
-                var commands = await client.GetGuildApplicationCommand(guildAppOptions.Application.ApplicationId, guildAppOptions.Guild.GuildId, commId).ConfigureAwait(false);
+                var commands = await client.GetGuildApplicationCommand(guildAppOptions.Application.ApplicationId, guildAppOptions.Guild.GuildId, commId, cancellationToken).ConfigureAwait(false);
 
                 invocationContext.Console.Append(new JsonView<ApplicationCommand>(commands));
             }
             else
             {
-                var commands = await client.GetGuildApplicationCommands(guildAppOptions.Application.ApplicationId, guildAppOptions.Guild.GuildId).ConfigureAwait(false);
+                var commands = await client.GetGuildApplicationCommands(guildAppOptions.Application.ApplicationId, guildAppOptions.Guild.GuildId, cancellationToken).ConfigureAwait(false);
 
                 var tableView = new ApplicationCommandTableView(commands);
 
--- src/DiscordDotNet.CommandLine/Commands/GatewayCommand.cs
+++ src/DiscordDotNet.CommandLine/Commands/GatewayCommand.cs
@@ -6,6 +6,7 @@ using System.CommandLine.Invocation;
 using System.CommandLine.Rendering;
 using System.CommandLine.Rendering.Views;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DiscordDotNet.CommandLine.Commands
@@ -14,13 +15,13 @@ namespace DiscordDotNet.CommandLine.Commands
     {
         public GatewayCommand() : base("gateway", "Get the gateway")
         {
-            Handler = CommandHandler.Create<InvocationContext, IHost>(Exec);
+            Handler = CommandHandler.Create<InvocationContext, IHost, CancellationToken>(Exec);
         }
 
-        private static async Task<int> Exec(InvocationContext invocationContext, IHost host)
+        private static async Task<int> Exec(InvocationContext invocationContext, IHost host, CancellationToken cancellationToken)
         {
             var client = host.Services.GetRequiredService<IDiscordWebClient>();
-            var gateway = await client.GetGateway().ConfigureAwait(false);
+            var gateway = await client.GetGateway(cancellationToken).ConfigureAwait(false);
 
             invocationContext.Console.Append(new ContentView(gateway.ToString()));
 
