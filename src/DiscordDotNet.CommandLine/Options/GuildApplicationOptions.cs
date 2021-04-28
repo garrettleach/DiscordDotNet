0d138413edb3bf77b40f0b90c4afa04d6a389fa5
Garrett Leach <garrett.d.leach@gmail.com>
2021-04-27 21:57:22 -0500

    Move to Options objects for some ids
    It makes sense to deal with objects where possible instead of strings to allow the type system to work with the developer.

---
+++ src/DiscordDotNet.CommandLine/Options/GuildApplicationOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace DiscordDotNet.CommandLine.Options
+{
+    internal class GuildApplicationOptions
+    {
+        public GuildApplicationOptions(ApplicationOptions application, GuildOptions guild)
+        {
+            Guild = guild;
+            Application = application;
+        }
+
+        public ApplicationOptions Application { get; }
+
+        public GuildOptions Guild { get; }
+    }
+}
